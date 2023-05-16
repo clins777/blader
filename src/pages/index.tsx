8d78240320763ae840c06d1196657ff9cdc4be42
Caio Lins <wolflow429@gmail.com>
2023-05-16 15:13:21 +0900

    remove auth for now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,8 @@
-import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
-
 import { api } from "~/utils/api";
 
-const CreateSpotWizard = () => {
-  const { user } = useUser();
-  if (!user) return null;
-  return (
-    <div className="flex">
-      <img
-        src={user.profileImageUrl}
-        alt="Profile image"
-        className="h-14 w-14 rounded-full"
-      />
-    </div>
-  );
-};
-
 const Home: NextPage = () => {
-  const user = useUser();
   const { data, isLoading } = api.spots.getAll.useQuery();
 
   if (isLoading) return <div>Loading spots ⏳</div>;
@@ -35,21 +18,8 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex h-screen justify-center">
         <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
-          <div className="flex border-b border-slate-400 p-4">
-            {!user.isSignedIn && (
-              <div className="flex justify-center">
-                <SignInButton />
-              </div>
-            )}
-            {/* {user.isSignedIn && (
-              <div className="flex justify-center">
-                <UserButton />
-              </div>
-            )} */}
-            {user.isSignedIn && <CreateSpotWizard />}
-          </div>
           <div className="flex flex-col">
-            {data.map((spot) => (
+            {data?.map((spot) => (
               <div key={spot.id} className="border-b border-slate-400 p-8">
                 {spot.spotName}
               </div>
