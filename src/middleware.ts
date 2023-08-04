209f9f2b71df79c84d73124a96c10b92b0de8181
Caio Lins <clins7777777@gmail.com>
2023-08-04 14:33:24 +0700

    adding an empty middleware afterAuth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({ publicRoutes: ["/"] });
+export default authMiddleware({
+  publicRoutes: ["/"],
+  afterAuth: () => console.log("Well this is annoying ..."),
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
